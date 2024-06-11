46be67b4cd4405c019c2b4c4c58246485e855c82
sonu <sonu78046@gmail.com>
2024-06-11 21:34:59 +0530

    pom changes

--- src/test/java/test/TestRunner.java
+++ src/test/java/test/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/feature",glue= {"stepDefination"},
-				plugin = {"pretty", "html:target/cucumber.html"})
+				plugin = "json:target/jsonReports/cucumber-report.json")
 public class TestRunner {
 //tags= ("@AddPlace" and "@DeletePlace"),
 }
